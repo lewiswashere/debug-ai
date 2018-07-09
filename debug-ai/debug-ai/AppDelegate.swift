130a9ec98e571778a3b954b2f0ec85e04a33dda0
Lewis Gardner <lewiswashere@gmail.com>
2018-07-09 14:05:18 -0500

    Created a root window & root view

--- debug-ai/debug-ai/AppDelegate.swift
+++ debug-ai/debug-ai/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        let view = ViewController()
+        window?.rootViewController = view
         return true
     }
 
