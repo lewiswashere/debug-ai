96be97a25ea4ced3f07fcfe1f1d11fb98399147c
Lewis Gardner <lewiswashere@gmail.com>
2018-07-09 13:55:18 -0500

    Created new main view controller in swift

---
+++ debug-ai/debug-ai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  debug-ai
+//
+//  Created by Lewis Gardner on 7/9/18.
+//  Copyright © 2018 Thought Experiment, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
