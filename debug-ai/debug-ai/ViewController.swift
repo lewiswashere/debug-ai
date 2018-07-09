b598b5d4f455ed03eef99a8f6849e4149da3c9fa
Lewis Gardner <lewiswashere@gmail.com>
2018-07-09 14:24:31 -0500

    Added a debug if no devices available

--- debug-ai/debug-ai/ViewController.swift
+++ debug-ai/debug-ai/ViewController.swift
@@ -25,7 +25,9 @@ class ViewController: UIViewController {
             if let device = devices.first {
                 session.addInput(try AVCaptureDeviceInput(device: device))
             }
-        } 
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
